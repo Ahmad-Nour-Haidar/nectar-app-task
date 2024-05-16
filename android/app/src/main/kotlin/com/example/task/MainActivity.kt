abbb21596fffff11ac0abff8f828816cf2bb9b28
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-05-16 19:26:20 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/task/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/constant/app_color.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+abstract final class AppColor {
+  static const primary = Color(0xff53B175);
+}
